709e5cec7591c7f93d9f4971bb9841f5bb7b9095
Hindu Muppala <120579608+hindu-muppala@users.noreply.github.com>
2023-02-18 15:38:33 +0530

    Update volunteer.js

--- src/backend/model/volunteer.js
+++ src/backend/model/volunteer.js
@@ -1,7 +1,13 @@
 const mangoose=require('mongoose');
 const Schema=mangoose.Schema
 const volunteer =new Schema({
- name: String,
- password: String,
+ username: {
+        type: String,
+        required: true
+    },
+ password: {
+    type: String,
+    required: true
+ }
 })
 module.exports =mangoose.model('volunteer',volunteer) // name of the schema is volunteer
