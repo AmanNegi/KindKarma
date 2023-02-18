0ebb21b85d033092e72ac2116f2dc8b6eda3f87a
Hindu Muppala <120579608+hindu-muppala@users.noreply.github.com>
2023-02-18 15:41:55 +0530

    Update organizer.js

--- src/backend/model/organizer.js
+++ src/backend/model/organizer.js
@@ -1,7 +1,13 @@
 const mangoose=require('mongoose');
 const Schema=mangoose.Schema
 const organizer =new Schema({
- name: String,
- password: String,
+username:{
+  type: String,
+  required: true
+},
+password:{
+ type: String,
+ required: true
+}
 })
 module.exports =mangoose.model('organizer',organizer) // name of the schema is volunteer
