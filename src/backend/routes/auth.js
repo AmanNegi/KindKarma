05e02f6ffddf05a3537f01b6a4482c8d9018cd01
amannegi <akuro787898@gmail.com>
2023-02-19 01:22:21 +0530

    fix(Backend): JSON Response fixed

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -4,7 +4,7 @@ const Organizer = require("../model/organizer");
 const Volunteer = require("../model/volunteer");
 
 router.get("/", async (req, res) => {
-  return res.send("Let's authenticate");
+  return res.json("Let's authenticate");
 });
 
 router.get("/login/organizer", async (req, res) => {
@@ -12,7 +12,7 @@ router.get("/login/organizer", async (req, res) => {
   const check = await Organizer.find({ email: req.query.email }).exec(); // checking by email
   console.log(check);
   if (check.length === 0) {
-    return res.status(400).send("No Organizer matches these details");
+    return res.status(400).json("No Organizer matches these details");
   }
 
   if (check[0].password === req.params.password);
@@ -24,7 +24,7 @@ router.get("/login/organizer", async (req, res) => {
     // if any body know better idea on session just implement
     // req.session.user = user;
     // ***** here sesion code ***
-    res.status(200).send(check[0]); // or use findOne method
+    res.status(200).json(check[0]); // or use findOne method
     return;
   }
 });
@@ -34,7 +34,7 @@ router.get("/login/volunteer", async (req, res) => {
   const check = await Volunteer.find({ email: req.query.email }).exec(); // checking by email
   console.log(check);
   if (check.length === 0) {
-    return res.status(400).send("No Volunteer matches these details");
+    return res.status(400).json("No Volunteer matches these details");
   }
 
   if (check[0].password === req.params.password);
@@ -46,7 +46,7 @@ router.get("/login/volunteer", async (req, res) => {
     // if any body know better idea on session just implement
     // req.session.user = user;
     // ***** here sesion code ***
-    res.status(200).send(check[0]); // or use findOne method
+    res.status(200).json(check[0]); // or use findOne method
     return;
   }
 });
@@ -56,11 +56,11 @@ router.post("/signup/volunteer", async (req, res) => {
 
   const response = await Organizer.find({ email: req.body.email }).exec();
   if (response.length > 0) {
-    return res.status(400).send("A User with this email already exists");
+    return res.status(400).json("A User with this email already exists");
   }
   const newVolunteer = new Volunteer(req.body);
   await newVolunteer.save();
-  return res.status(200).send("Congrats! You have signed in successfully!");
+  return res.status(200).json("Congrats! You have signed in successfully!");
 });
 
 router.post("/signup/organizer", async (req, res) => {
@@ -68,17 +68,17 @@ router.post("/signup/organizer", async (req, res) => {
   const check = await Organizer.find({ email: req.body.email }).exec();
 
   if (check.length > 0) {
-    return res.status(400).send("A User with this email already exists");
+    return res.status(400).json("A User with this email already exists");
   }
   const newOrganizer = new Organizer(req.body);
   await newOrganizer.save();
-  return res.status(200).send("Congrats! You have signed in successfully!");
+  return res.status(200).json("Congrats! You have signed in successfully!");
 });
 
 //TODO: Implement this method
 router.get("/logout", async (req, res) => {
   // if any know how to destroy session implement
-  res.send("logOut");
+  res.json("logOut");
 });
 
 function emailPasswordCheck(email, pass) {
