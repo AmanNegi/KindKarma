6b315d64584326fb636acee37f60b31fa4ce5908
Hindu Muppala <120579608+hindu-muppala@users.noreply.github.com>
2023-02-19 18:50:22 +0530

    Add files via upload

--- src/fontend2/KindKarma/src/components/SignUp/SignUp.js
+++ src/fontend2/KindKarma/src/components/SignUp/SignUp.js
@@ -1,1 +1,17 @@
+import React, { useState } from "react";
 
+export default function SignUp() { 
+  const [inputValue, setInputValue] = useState(""); 
+  const handleInputChange = (e) => { 
+    setInputValue(e.target.value) 
+  } 
+  const handleSubmitButton = () => { 
+    alert(inputValue); 
+  }; 
+  return ( 
+    <div className="App"> 
+      <input value={inputValue} onChange={handleInputChange} /> 
+      <input type="submit" value="submit" onClick={handleSubmitButton} /> 
+    </div> 
+  ); 
+}
